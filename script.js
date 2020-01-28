0dbf76562688a2829a0fd7c1a5c4d8d9aec7bf52
Arlene Sanchez <arlene.sanchez004@gmail.com>
2020-01-27 22:12:33 -0500

    Quiz starts on button press, score is affected by correct answer, timer is affected by wrong answer, questions cycle upon answering, questions are checked for correctness.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var highscores = document.getElementById("highscores");
+var newScore = document.getElementById("newScore");
 var timer = document.getElementById("timer");
 var start = document.getElementById("start");
 var quiz = document.getElementById("quiz");
@@ -7,24 +8,94 @@ var choiceA = document.getElementById("choiceA");
 var choiceB = document.getElementById("choiceB");
 var choiceC = document.getElementById("choiceC");
 
-var secondsLeft = 120;
+let questions = [
+    {
+        question: "hi this is Q1",
+        choiceA: "Correct",
+        choiceB: "Wrong",
+        choiceC: "Wrong",
+        correct: "choiceA",
+    },
+    {
+        question: "second question",
+        choiceA: "Wrong",
+        choiceB: "Correct",
+        choiceC: "Wrong",
+        correct: "choiceB",
+    },
+    {
+        question: "beep bop third Question",
+        choiceA: "Wrong",
+        choiceB: "Wrong",
+        choiceC: "Correct",
+        correct: "choiceC",
+    },
+    {
+        question: "Final Question",
+        choiceA: "Correct",
+        choiceB: "Wrong",
+        choiceC: "Wrong",
+        correct: "choiceA",
+    },
+];
 
+var finalQuestion = questions.length - 1;
+var currentQuestion = 0;
+var score = 0;
+var secondsLeft = 60;
+
+
+function showQuestion() {
+    var q = questions[currentQuestion];
+    question.innerHTML = "<h2>" + q.question + "</h2>"
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
 function setTime() {
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timer.textContent = "Time: " + secondsLeft;
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timer.textContent = "Time: " + secondsLeft;
 
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval);
-      //sendMessage();
+        if (secondsLeft <= 0) {
+            clearInterval(timerInterval);
+            quiz.style.display = "none";
+            
+        }
+    }, 1000);
+}
+function startQuiz() {
+    start.style.display = "none";
+    quiz.style.display = "block";
+    showQuestion();
+    setTime();
+    console.log("hey");
+}
+function check(answer) {
+    if (answer == questions[currentQuestion].correct) {
+        score += 10;
+        console.log("correct " + score);
+        currentQuestion++;
+    } else {
+        secondsLeft -= 10;
+        currentQuestion++;
     }
-  }, 1000);
+
+    if (currentQuestion <= finalQuestion) {
+        showQuestion();
+        console.log(currentQuestion);
+    } else {
+        secondsLeft = 0;
+        gameOver();
+
+    }
 }
 
-// function sendMessage() {
-//   timeEl.textContent = " ";
-//   var imgEl = document.createElement("img");
-//   mainEl.appendChild(imgEl);
-// }
+function gameOver(){
+    newScore.style.display = "block";
+}
 
-setTime();
+start.addEventListener("click", startQuiz);
+
+
+
